083fed04103808b2ea937f77bb8e6fbef8e5d8fa
losbiw <skorinov41@gmail.com>
2020-03-31 00:08:47 +0300

    add existing files

--- script.js
+++ script.js
@@ -1,66 +1,30 @@
-window.onload = function(){
-    var buttons = document.querySelectorAll('.quotes .buttons button');
-    console.log(buttons[3]);
-    var portfolio = document.querySelectorAll('.portfolio .container div');
-    $('.top .navbar .links a').hover(
-        function(){
-            $(this).css('color', '#10c9c3');
-            $(this).css('font-size', '16px');
-            $(this).css('transition', '800ms');
-        },
-        function(){
-            $(this).css('color', 'black');
-            $(this).css('font-size', '15px');
-            $(this).css('transition', '800ms');
-        }
-    );
+const portoflioItems = $('#portfolio .item');
 
-    $(portfolio).hover(
-        function(){
-            var link = jQuery('a', this);
-            var overlay = jQuery('.overlay', this);
-            $(this).css('transition', '600ms');
-            $(link).css('display', 'block');
-            $(overlay).css('opacity', '1');
-        },
-        function(){
-            var link = jQuery('a', this);
-            var overlay = jQuery('.overlay', this);
-            $(link).css('display', 'none');
-            $(overlay).css('opacity', '0');
-        }
-    );
+$(portoflioItems).hover(function(){
+    $('.overlay', this).css('display', 'flex');
+}, function(){
+    $('.overlay', this).css('display', 'none');
+});
 
-    $(buttons).click(function(){
-        $(buttons).css('width', '8px');
-        $(buttons).css('height', '12px');
-        $(buttons).css('border-radius', '6px');
-        $(buttons).css('background', '#d8d8d8');
-        $(this).css('width', '20px');
-        $(this).css('height', '20px');
-        $(this).css('border-radius', '10px');
-        $(this).css('background', 'white');
-        $(this).css('transition', '500ms');
-    });
+changeQuote();
 
-    $(buttons[0]).click(function(){
-        $('.quotes .container q').text('Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program');
-        $('.quotes .container p').text('linus torvalds');
+function changeQuote(){
+    const quotes = [{
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        author: 'Albert Einstein'
+    },
+    {
+        quote: 'Outstanding job and exceeded all expectations. It was a pleasure to work with them on a sizable first project and am looking forward to start the next one asap.',
+        author: 'michael hopkins'
+    },
+    {
+        quote: 'Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.',
+        author: 'martin luther king jr.'
+    }];
+    const buttons = $('#quotes button');
+    buttons.click(function(){
+        const id = $(this).attr('id');
+        $('#quotes q').text(quotes[id].quote);
+        $('#quotes p').text(quotes[id].author);
     });
-    $(buttons[1]).click(function(){
-        $('.quotes .container q').text('Outstanding job and exceeded all expectations. It was a pleasure to work with them on a sizable first project and am looking forward to start the next one asap.');
-        $('.quotes .container p').text('michael hopkins');
-    });
-    $(buttons[2]).click(function(){
-        $('.quotes .container q').text('Get closer than ever to your customers. So close that you tell them what they need well before they realize it themselves.');
-        $('.quotes .container p').text('steve jobs');
-    });
-
-    $('.input .container button').click(function(){
-        $('#name').val("");
-        $('#email').val("");
-        $('#title').val("");
-        $('#com').val("");
-    });
-    AOS.init();
 }
